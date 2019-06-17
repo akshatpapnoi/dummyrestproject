6af45318ac43ce659f4580fc09435a522da26330
akshatpapnoi <akshatpapnoi1110@gmail.com>
2019-06-17 09:13:04 +0530

    major one

--- dummy/settings.py
+++ dummy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '#x_0kc0hwnc&ah4*c-d(yfatra#ns3agaj6++95#g_42+lc%rp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['halanx-hiring.herokuapp.com']
 
 
 # Application definition
